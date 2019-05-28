0d78e4cd84fb6f8db65cca4647a554b69abdf563
tomozip <dev.tomoki@gmail.com>
2019-05-28 11:23:16 +0900

    initial commit

---
+++ my-template/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  my-template
+//
+//  Created by 島田智貴 on 2019/05/28.
+//  Copyright © 2019 Tomozip. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
